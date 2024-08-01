653fab3ffb31d407cb687da4f281a37b8a2eefea
ckr <ckr3453@gmail.com>
2024-08-01 16:38:29 +0900

    init project

---
+++ src/main/kotlin/kr/movements/lunchorder/LunchOrderApplication.kt
@@ -0,0 +1,11 @@
+package kr.movements.lunchorder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LunchOrderApplication
+
+fun main(args: Array<String>) {
+    runApplication<LunchOrderApplication>(*args)
+}
---
+++ src/test/kotlin/kr/movements/lunchorder/LunchOrderApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.movements.lunchorder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LunchOrderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
