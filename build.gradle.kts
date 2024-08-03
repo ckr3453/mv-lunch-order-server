2b4735749a5b9b99ecd7bfd8b1cfdd6b9c0f7f1c
ckr <ckr3453@gmail.com>
2024-08-04 02:22:54 +0900

    chore: 플러그인 및 의존성에 대한 버전 별도 명시

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
-    id("org.springframework.boot") version "3.3.2"
-    id("io.spring.dependency-management") version "1.1.6"
-    kotlin("plugin.jpa") version "1.9.24"
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
+    val springBootVersion = "3.3.2"
+    val springBootDependencyManagementVersion = "1.1.6"
+    val kotlinVersion = "1.9.24"
+
+    id("org.springframework.boot") version springBootVersion
+    id("io.spring.dependency-management") version springBootDependencyManagementVersion
+    kotlin("plugin.jpa") version kotlinVersion
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
 }
 
 allOpen {
@@ -38,27 +42,31 @@ repositories {
 }
 
 dependencies {
+    annotationProcessor("org.projectlombok:lombok")
+
+    compileOnly("org.projectlombok:lombok")
+
+//    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.h2database:h2")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    compileOnly("org.projectlombok:lombok")
-//    runtimeOnly("org.postgresql:postgresql")
-    implementation("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("io.jsonwebtoken:jjwt-api:${DependencyVersion.JJWT}")
+    implementation("io.jsonwebtoken:jjwt-impl:${DependencyVersion.JJWT}")
+    implementation("io.jsonwebtoken:jjwt-jackson:${DependencyVersion.JJWT}")
+    implementation("com.google.code.gson:gson:${DependencyVersion.GSON}")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${DependencyVersion.SPRINGDOC}")
 
-    annotationProcessor("org.projectlombok:lombok")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
-    implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
-    implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
-    implementation("com.google.code.gson:gson:2.11.0")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 }
 
 kotlin {
@@ -69,4 +77,10 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+object DependencyVersion {
+    const val JJWT = "0.12.6"
+    const val GSON = "2.11.0"
+    const val SPRINGDOC = "2.6.0"
 }
