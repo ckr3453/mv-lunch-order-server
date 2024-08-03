c26ea1545bbadd91965b4daf2d48d9971d26be54
ckr <ckr3453@gmail.com>
2024-08-04 00:53:59 +0900

    feat: 인증 성공 후처리 핸들러 추가

---
+++ src/main/kotlin/kr/movements/lunchorder/handler/AuthenticationSuccessHandlerImpl.kt
@@ -0,0 +1,40 @@
+package kr.movements.lunchorder.handler
+
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import kr.movements.lunchorder.component.JwtProvider
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+
+/**
+ * packageName : kr.movements.lunchorder.handler
+ * fileName    : OAuth2SuccessHandler
+ * author      : ckr
+ * date        : 24. 8. 2.
+ * description :
+ */
+
+class AuthenticationSuccessHandlerImpl(
+    private val jwtProvider: JwtProvider
+): AuthenticationSuccessHandler {
+
+    override fun onAuthenticationSuccess(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authentication: Authentication
+    ) {
+        val accessToken = jwtProvider.createToken(authentication)
+        response.addCookie(createTokenCookie("a", accessToken))
+        response.addCookie(createTokenCookie("r", accessToken))
+        response.sendRedirect("${request.contextPath}/swagger-ui/index.html")
+    }
+
+    private fun createTokenCookie(name: String, token: String): Cookie {
+        return Cookie(name, token).apply {
+            this.path = "/"
+            this.isHttpOnly = true
+            this.maxAge = 600
+        }
+    }
+}
